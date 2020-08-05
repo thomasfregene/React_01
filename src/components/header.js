d45ff964143d636eaff13aa451e4573aae96efaf
thomasfregene <fregzythomas@gmail.com>
2020-08-05 18:31:42 +0100

    Adding styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,24 @@
 import React,{Component} from 'react';
 
 
+
+// let styles ={
+//     header:{
+//         backgroundColor:'#03a9f4'
+//     },
+//     logo:{
+//         color:'#fff',
+//         fontFamily: 'Roboto',
+//         textAlign: 'center'
+//     }
+// }
+
 class Header extends Component{
     render(){
         return(
             <header>
-                <div>Logo</div>
-                <input/>
+                <div className='logo'>Logo</div>
+                <input/> 
             </header>
         )
     }
